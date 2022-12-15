067038f4a88d3c1a6ed080b3de144ed069216913
moise <moisemlg90@gmail.com>
2022-12-15 14:58:04 +0200

    Generate artist controller with the following actions index to get data from the database show to display data on the view new to create a new artist delete to remove any artist on the list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'artists/index'
+  get 'artists/show'
+  get 'artists/new'
+  get 'artists/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
