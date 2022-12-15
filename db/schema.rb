8825c8e49891764cb85e6c38a17731750725bb1d
moise <moisemlg90@gmail.com>
2022-12-15 16:04:52 +0200

    Migrate the artist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_130715) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_140100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.integer "numbers_of_album"
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_artists_on_genre_id"
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -20,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_130715) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "artists", "genres"
 end
