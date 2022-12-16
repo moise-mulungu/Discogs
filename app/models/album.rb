f546c23c818cfe9a089e26b19744b0fe0795f4cb
moise <moisemlg90@gmail.com>
2022-12-16 22:49:28 +0200

    Add validations to the song model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :artist
-  
+  has_many :songs, dependent: :destroy
+
   validates :name, presence: true
   validates :artist_id, presence: true
   validates :number_of_songs, presence: true
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :album
+
+  validates :name, presence: true
+  validates :album_id, presence: true
 end
