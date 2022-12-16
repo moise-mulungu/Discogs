dc785ca6091c0c74b95bfe104c71369c9201ce1c
moise <moisemlg90@gmail.com>
2022-12-16 22:37:29 +0200

    Migrate the album table into the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_193940) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_203128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.date "released_year"
+    t.integer "number_of_songs"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -31,5 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_193940) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "albums", "artists"
   add_foreign_key "artists", "genres"
 end
