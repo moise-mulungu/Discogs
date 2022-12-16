6e2647a69ef0a7709e2942bf6130ca983807303f
moise <moisemlg90@gmail.com>
2022-12-16 23:01:39 +0200

    Nest resource routes of genres, artists, albums, and songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'songs/index'
-  get 'songs/show'
-  get 'songs/new'
-  get 'songs/delete'
-  resources :albums
-  # get 'genres/index'
- 
-  resources :genres
- 
-  resources :artists
+  
+  resources :genres do
+    resources :artists, only: [:new, :create]
+  end
+  
+  resources :artists do
+    resources :albums, only: [:new, :create]
+  end
+  
+  resources :artists, only: [:edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :albums do
+    resources :songs, only: [:new, :create]
+  end
+  resources :songs, only: [:edit, :update, :destroy]
 
   # Defines the root path route ("/")
   root "genres#index"
