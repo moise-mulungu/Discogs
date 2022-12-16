aaa9460f890b8b0e4509ecce2a8dbc8d0bac6266
moise <moisemlg90@gmail.com>
2022-12-16 22:24:05 +0200

    Update the album routes with resources key word

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'albums/index'
-  get 'albums/show'
-  get 'albums/new'
-  get 'albums/delete'
+  resources :albums
   # get 'genres/index'
  
   resources :genres
